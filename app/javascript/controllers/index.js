3b3268daef6c7792b76ff581a2dd631c8932ab8c
Taimoor Qureshi <taimoorq@gmail.com>
2026-03-09 11:22:31 -0400

    Automated bundle update and yarn upgrade

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,22 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import CopyController from "controllers/copy_controller"
+import CountupController from "controllers/countup_controller"
+import HelloController from "controllers/hello_controller"
+import InboxController from "controllers/inbox_controller"
+import LocalTimeController from "controllers/local_time_controller"
+import NavController from "controllers/nav_controller"
+import ProjectSearchController from "controllers/project_search_controller"
+import RevealController from "controllers/reveal_controller"
+import ScreenshotsSliderController from "controllers/screenshots_slider_controller"
+import TabsController from "controllers/tabs_controller"
+
+application.register("copy", CopyController)
+application.register("countup", CountupController)
+application.register("hello", HelloController)
+application.register("inbox", InboxController)
+application.register("local-time", LocalTimeController)
+application.register("nav", NavController)
+application.register("project-search", ProjectSearchController)
+application.register("reveal", RevealController)
+application.register("screenshots-slider", ScreenshotsSliderController)
+application.register("tabs", TabsController)
